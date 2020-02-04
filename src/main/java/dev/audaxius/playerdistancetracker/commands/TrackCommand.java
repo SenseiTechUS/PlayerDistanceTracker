b85b0270593a06b0dfb0753205c6346d57ab2a4b
SenseiTechUS <mahrbazlee@gmail.com>
2020-02-04 15:25:13 -0500

    Completed base plugin

--- src/main/java/dev/audaxius/playerdistancetracker/commands/TrackCommand.java
+++ src/main/java/dev/audaxius/playerdistancetracker/commands/TrackCommand.java
@@ -3,16 +3,50 @@ package dev.audaxius.playerdistancetracker.commands;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.*;
 import dev.audaxius.playerdistancetracker.PlayerDistanceTracker;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Server;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @CommandAlias("track|playerdistancetracker|pdt")
 public class TrackCommand extends BaseCommand {
 
-    @Default
-    @Syntax("/track ")
-    @Description("Track nearest player's distance")
-    public static void onTrack(Player player){
+    private static PlayerDistanceTracker instance = PlayerDistanceTracker.getInstance();
+    private static Server server = instance.getServer();
+    public static Map<Player,Player> trackers = new HashMap<>();
 
+    @Default
+    @Syntax("/track [target]")
+    @Description("Track nearest player's distance")
+    @CommandCompletion("@players")
+    public static void onTrack(Player player, @Optional String target) {
+        if (target != null) {
+            if(server.getPlayer(target) != null){
+                trackers.put(player,server.getPlayer(target));
+                player.sendMessage(ChatColor.AQUA + "Tracking " + ChatColor.WHITE + target);
+            }else{
+                player.sendMessage(ChatColor.RED + target + " not found!");
+                return;
+            }
+        }else{
+            double distance = Double.MAX_VALUE;
+            Player closestTarget = player;
+            for(Player cTarget : server.getOnlinePlayers()){
+                double distanceTemp = player.getLocation().distance(cTarget.getLocation());
+                if(distanceTemp < distance && distanceTemp != 0){
+                    distance = distanceTemp;
+                    closestTarget = cTarget;
+                }
+            }
+            trackers.put(player,closestTarget);
+            player.sendMessage(ChatColor.AQUA + "Tracking " + ChatColor.WHITE + closestTarget.getDisplayName());
+        }
+    }
+
+    public static Map<Player, Player> getTrackers() {
+        return trackers;
     }
 }
--- src/main/java/dev/audaxius/playerdistancetracker/events/CompassEvent.java
+++ src/main/java/dev/audaxius/playerdistancetracker/events/CompassEvent.java
@@ -1,7 +1,35 @@
 package dev.audaxius.playerdistancetracker.events;
 
+import dev.audaxius.playerdistancetracker.commands.TrackCommand;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Map;
 
 public class CompassEvent implements Listener {
 
+    private static Map<Player, Player> trackers = TrackCommand.getTrackers();
+
+    @EventHandler
+    public void onHoldCompass(PlayerItemHeldEvent event) {
+        Player player = event.getPlayer();
+        ItemStack item = player.getInventory().getItem(event.getNewSlot());
+
+        ItemStack compass = new ItemStack(Material.COMPASS);
+        if(item == null){
+            return;
+        }
+        if (item.isSimilar(compass)) {
+            if (trackers.containsKey(player)) {
+                Player target = trackers.get(player);
+                int distance = (int)player.getLocation().distance(target.getLocation());
+                player.sendActionBar(ChatColor.WHITE + target.getDisplayName() + ChatColor.AQUA + " is " + ChatColor.WHITE + distance + ChatColor.AQUA + " blocks away");
+            }
+        }
+    }
 }
